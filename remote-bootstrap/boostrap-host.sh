b9b04591b129572159f1918a69139ae695b86a57
Nate Brady <nate@ultimus.cloud>
2024-05-16 08:36:51 -0500

    update

--- remote-bootstrap/boostrap-host.sh
+++ remote-bootstrap/boostrap-host.sh
@@ -1,7 +1,7 @@
 #Manual run:
 
-export HOST_IP=3.219.56.195
+export HOST_IP=1.2.3.4
 export HOST_SSH_USER=ubuntu
-export HOST_SSH_KEY=/home/terratrax/metal/metaltest1.pem
+export HOST_SSH_KEY=/path/to/key
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 export K3S_VERSION=v1.28.7+k3s1
